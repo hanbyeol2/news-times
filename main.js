ab63b29481e63633b5864fe426bd31e4a406e5bb
hanbyeol <dlgksquf8535@gmail.com>
2024-07-04 22:09:32 +0900

    pagination

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ let url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 );
 
+let totalResults = 0;
+let page = 1;
+const pageSize = 10;
+const groupSize = 5;
+
 const getNews = async()=>{
     try{
+        url.searchParams.set("page",page); 
+        url.searchParams.set("pageSize", pageSize);
         const response = await fetch(url);
         const data = await response.json();
         if(response.status===200){
@@ -15,7 +22,9 @@ const getNews = async()=>{
                 throw new Error("No result for this search");
             }
             newsList = data.articles;
+            totalResults = data.totalResults;
             render();
+            paginationRender();
         }else{
             throw new Error(data.message);
         }
@@ -73,6 +82,50 @@ const errorRender = (errorMassage) => {
    document.getElementById('news-board').innerHTML = errorHTML;
 }
 
+const paginationRender=()=>{
+    //totalResult
+    //page
+    //pagesize
+    //groupsize
+    //pagegroup
+    const totalPages = Math.ceil(totalResults/pageSize);
+    const pageGroup = Math.ceil(page / groupSize);
+    //lastPage
+    let lastPage = pageGroup * groupSize;
+    if(lastPage > totalPages){
+        lastPage = totalPages;
+    }
+    //firstPage
+    const firstPage = lastPage - (groupSize - 1)<=0? 1 : lastPage - (groupSize - 1);
+    let paginationHTML = ``;
+    if(firstPage >= 6){
+        paginationHTML = `<li class="page-item" onclick="moveToPage(1)"><a class="page-link">&lt;&lt;</a></li><li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link">&lt;</a></li>`;
+    }
+    for(let i = firstPage; i <= lastPage;i++){
+        paginationHTML += `<li class="page-item ${i===page?"active" : ""}" onclick="moveToPage(${i});"><a class="page-link">${i}</a></li>`;
+    }
+    if(lastPage < totalPages){
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link">&gt;</a></li> <li class="page-item" onclick="moveToPage(${lastPage})"><a class="page-link">&gt;&gt;</a></li>`;
+    }
+
+
+    document.querySelector('.pagination').innerHTML=paginationHTML;
+
+//     <nav aria-label="Page navigation example">
+//   <ul class="pagination">
+//     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+//     <li class="page-item"><a class="page-link" href="#">1</a></li>
+//     <li class="page-item"><a class="page-link" href="#">2</a></li>
+//     <li class="page-item"><a class="page-link" href="#">3</a></li>
+//     <li class="page-item"><a class="page-link" href="#">Next</a></li>
+//   </ul>
+// </nav>
+}
+
+const moveToPage=(pageNum)=>{
+    page=pageNum;
+    getNews()
+}
 getLatestNews();
 
 
